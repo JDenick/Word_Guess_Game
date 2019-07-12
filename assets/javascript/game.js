0ea662c9acab67e7eeacdf7560f5be408377f845
Jesse Denick <jessedenick@gmail.com>
2019-07-12 14:09:06 -0400

    formatted index and css files for basic game design and included reset css file

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+// Create an array of Words
+// Choose word randomly
+// Create underscores based on length of word
+// Get users guess
+// Check if guess is right
+// If right push to right array
+// If wrong push to wrong array
