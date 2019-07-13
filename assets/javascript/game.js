8f523da8233b7ab098d5ce9918242b4eb101b723
Jesse Denick <jessedenick@gmail.com>
2019-07-13 00:43:57 -0400

    finished coding basic game of word guess in javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -44,7 +44,79 @@ function startGame () {
     console.log(blanksAndSuccesses);
 }
 
+function checkLetters(letter) {
+    // Check if letter exists in code at all
+
+    var isLetterInWord = false;
+
+    for (var i=0; i<numBlanks; i++){
+        if(selectedWord[i] == letter){
+            isLetterInWord = true;
+        }
+    }
+
+    // Check where in the word the letter exists, then populate our blanksAndSuccesses array
+    if(isLetterInWord) {
+        for (var i=0; i<numBlanks; i++) {
+            if (selectedWord[i] == letter) {
+                blanksAndSuccesses[i] = letter;
+            }
+        }
+    }
+
+    // Letter wasnt found
+    else {
+        wrongLetters.push(letter);
+        guessesLeft--
+    }
+
+    // Testing / Debugging
+    console.log(blanksAndSuccesses);
+
+}
+
+function roundComplete(){
+    console.log("Win Count: " + winCount + " | Loss Count: " +  lossCount + " | Guesses Left " + guessesLeft);
+
+    //Update the HTML to reflect the most recent count stats
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join("  ");
+    document.getElementById("wrongGuesses").innerHTML = wrongLetters.join("  ");
+    //
+    //Check if user won
+    if (lettersInWord.toString() == blanksAndSuccesses.toString()) {
+        winCount++
+        alert("You Won!");
+
+        //Update the win counter in the HTML
+        document.getElementById("winCounter").innerHTML = winCount;
+
+        startGame();
+    }
+    //Check if user lost
+    else if (guessesLeft == 0) {
+        lossCount++
+        alert("You Lost...");
+
+        //Update the loss counter in the HTML
+        document.getElementById("lossCounter").innerHTML = lossCount;
+
+        startGame();
+    }
+}
+
 // MAIN PROCESS
 //--------------------------------
 
+// Initiate the code the first time
 startGame();
+
+// Register keyclicks
+document.onkeyup = function(event){
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    roundComplete();
+
+    // Testing / Debugging
+    console.log(letterGuessed);
+}
